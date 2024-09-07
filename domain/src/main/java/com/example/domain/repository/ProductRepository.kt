0df01d13cbbcc6dd9f0d3fb61958b35ab73afab5
Bernard Owusu Ansah <90481075+1Bernard@users.noreply.github.com>
2024-09-07 17:12:21 +0000

    add home screen and home view model

--- domain/src/main/java/com/example/domain/repository/ProductRepository.kt
+++ domain/src/main/java/com/example/domain/repository/ProductRepository.kt
@@ -4,5 +4,5 @@ import com.example.domain.model.Product
 import com.example.domain.network.ResultWrapper
 
 interface ProductRepository {
-    suspend fun getProduct(): ResultWrapper<List<Product>>
+    suspend fun getProducts(): ResultWrapper<List<Product>>
 }
